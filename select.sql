c5d2e5ef0ae8f85d5688813e07f1af9a83a5c57d
Amur4ek <147254093+Amur4ek@users.noreply.github.com>
2024-01-30 23:34:25 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -16,7 +16,7 @@ where artist_name not like '% %'
 
 select track_name
 from track 
-where track_name like '%my%'
+where track_name ilike '%my%'
 
 
 SELECT genre_name, COUNT(artists_and_genres.artist_id) 
@@ -36,9 +36,21 @@ from albums
 join artists_and_albums on albums.album_id = artists_and_albums.album_id
 GROUP BY albums.album_name;
 
-SELECT artists.artist_id _id, artists.artist_name _name
+SELECT artists.artist_id , artists.artist_name
 FROM artists
-JOIN artists_and_albums  ON artists.artist_id = artists_and_albums.artist_id
-JOIN albums ON albums.album_id = artists_and_albums.album_id
-WHERE albums.year_ != 2020 ;
+WHERE artist_id NOT IN (
+    SELECT artist_id
+    FROM artists_and_albums
+    JOIN albums ON artists_and_albums.album_id = albums.album_id
+    WHERE albums.year_ = 2020
+)
+  
+SELECT collection.collection_name , albums.album_name , artists.artist_name 
+from artists
+join artists_and_albums ON artists.artist_id = artists_and_albums.artist_id
+join albums on artists_and_albums.album_id = albums.album_id 
+join track on albums.album_id = track.album_id 
+join collection_and_track on track.track_id = collection_and_track.track_id 
+join collection on collection_and_track.collection_id = collection.collection_id 
+WHERE artists.artist_name ilike '%Скриптонит%' ;
 
